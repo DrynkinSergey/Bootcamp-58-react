6f986a3f20c02fcdc3f98c78b766f2d6ef3bfd0e
Sergey <sergsmile29@gmail.com>
2023-09-08 10:08:45 +0300

    synt Event, click

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import { Flex, FlexContainer, StyledButton, StyledCounter } from './Counter.styled'
 
 export const Counter = () => {
+	const sayHi = e => {
+		console.log(e)
+		console.log('Hello')
+	}
+
+	const grit = name => {
+		console.log('Hello ' + name + ' How are you?')
+	}
+	let count = 10
+	const inc = () => {
+		count += 1
+	}
 	return (
 		<FlexContainer>
 			<StyledCounter>
-				<h1>{0}</h1>
+				<h1 onClick={() => grit('Petya')}>{count}</h1>
 				<Flex>
-					<StyledButton>minus</StyledButton>
+					<StyledButton onClick={() => grit('Oleh')}>minus</StyledButton>
+
 					<StyledButton>reset</StyledButton>
-					<StyledButton>plus</StyledButton>
+					<StyledButton onClick={inc}>plus</StyledButton>
 				</Flex>
 			</StyledCounter>
 		</FlexContainer>
