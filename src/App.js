be1131c15806c6084c320fba150b1f5c31cf3a5b
Sergey <sergsmile29@gmail.com>
2023-09-11 10:44:14 +0300

    controlledForm

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import { Employee } from './components/Employees/Employee'
 import Login from './components/Login/Login'
 
 const App = () => {
+	const handleLogin = data => {
+		console.log('Form is submitting!....')
+		setTimeout(() => {
+			console.log('Data is ready to login')
+		}, 1000)
+		setTimeout(() => {
+			console.log(data)
+		}, 3000)
+	}
 	return (
 		<>
 			<StyledFlex $center>
-				<Login />
+				<RegisterForm login={handleLogin} />
 			</StyledFlex>
 			<Employee />
 		</>
