cda8bc36cd70a0d26488f9e226312c574781c442
Sergey <sergsmile29@gmail.com>
2023-09-15 11:02:00 +0300

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,55 +1,97 @@
-import { Component } from 'react'
+import { Component, useEffect, useRef } from 'react'
 import { CloseButton, ModalContent, ModalWrapper } from './Modal.styled'
 
-class Modal extends Component {
-	intervalId = null
-	timeoutId = null
-	handleKeyDown = e => {
-		if (e.key === 'Escape') {
-			this.props.close()
+// class Modal extends Component {
+// 	intervalId = null
+// 	timeoutId = null
+// 	handleKeyDown = e => {
+// 		if (e.key === 'Escape') {
+// 			this.props.close()
+// 		}
+// 	}
+// 	componentDidMount() {
+// 		document.body.style.overflow = 'hidden'
+// 		this.intervalId = setInterval(() => {
+// 			console.log(new Date().toLocaleTimeString())
+// 		}, 1000)
+
+// 		this.timeoutId = setTimeout(() => {
+// 			console.log('Hello')
+// 		}, 3000)
+
+// 		document.addEventListener('keydown', this.handleKeyDown)
+// 	}
+// 	componentWillUnmount() {
+// 		document.body.style.overflow = 'auto'
+
+// 		document.removeEventListener('keydown', this.handleKeyDown)
+// 		clearInterval(this.intervalId)
+// 		clearTimeout(this.timeoutId)
+// 		console.log('UNMOUNT')
+// 	}
+
+// 	onBackDropClick = e => {
+// 		if (e.currentTarget === e.target) {
+// 			this.props.close()
+// 		}
+// 	}
+
+// 	render() {
+// return (
+// 	<ModalWrapper onClick={this.onBackDropClick}>
+// 		<ModalContent>
+// 			<>
+// 				<h1>Modal</h1>
+// 				<hr />
+// 			</>
+// 			<CloseButton onClick={this.props.close}>×</CloseButton>
+// 			{this.props.children}
+// 		</ModalContent>
+// 	</ModalWrapper>
+// )
+// 	}
+// }
+
+const Modal = ({ children, close, title }) => {
+	const intervalID = useRef(null)
+
+	useEffect(() => {
+		const handleKeyDown = e => {
+			if (e.key === 'Escape') {
+				console.log('Escape')
+				close()
+			}
 		}
-	}
-	componentDidMount() {
+		document.addEventListener('keydown', handleKeyDown)
 		document.body.style.overflow = 'hidden'
-		this.intervalId = setInterval(() => {
+		intervalID.current = setInterval(() => {
 			console.log(new Date().toLocaleTimeString())
 		}, 1000)
 
-		this.timeoutId = setTimeout(() => {
-			console.log('Hello')
-		}, 3000)
-
-		document.addEventListener('keydown', this.handleKeyDown)
-	}
-	componentWillUnmount() {
-		document.body.style.overflow = 'auto'
-
-		document.removeEventListener('keydown', this.handleKeyDown)
-		clearInterval(this.intervalId)
-		clearTimeout(this.timeoutId)
-		console.log('UNMOUNT')
-	}
-
-	onBackDropClick = e => {
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown)
+			clearInterval(intervalID.current)
+			document.body.style.overflow = 'auto'
+			console.log('Не закривай мене')
+		}
+	}, [close])
+	const onBackDropClick = e => {
 		if (e.currentTarget === e.target) {
-			this.props.close()
+			close()
 		}
 	}
-
-	render() {
-		return (
-			<ModalWrapper onClick={this.onBackDropClick}>
-				<ModalContent>
-					<>
-						<h1>Modal</h1>
-						<hr />
-					</>
-					<CloseButton onClick={this.props.close}>×</CloseButton>
-					{this.props.children}
-				</ModalContent>
-			</ModalWrapper>
-		)
-	}
+	return (
+		<ModalWrapper onClick={onBackDropClick}>
+			<ModalContent>
+				<>
+					<h1>Modal</h1>
+					<hr />
+				</>
+				<CloseButton onClick={close}>×</CloseButton>
+				{children}
+			</ModalContent>
+		</ModalWrapper>
+	)
 }
 
 export default Modal
