2caccff30a6ebd40733240285f8a1b134e3d012a
Sergey <sergsmile29@gmail.com>
2023-09-18 10:15:55 +0300

    useCallback, useMemo

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,40 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Flex, FlexContainer, StyledButton, StyledCounter } from './Counter.styled'
-
+const data = ['1', 2, 3, 4, 5, 6, 11]
 export const Counter = () => {
 	const text = 'hello'
 	const [counter, setCounter] = useState(0)
 	const [step, setStep] = useState(1)
 	const [name, setName] = useState(text)
-	const [user, setUser] = useState({
-		name: 'alex',
-		email: 'alex@1.ua',
-	})
-	const countOfRenders = useRef(0)
-	useEffect(() => {
-		countOfRenders.current += 1
-		console.log('RENDER COUNT : ', countOfRenders.current)
-	})
 
-	useEffect(() => {
-		console.log('hello counter')
+	const sum = value => {
+		console.log('CALC SOME DATA')
+		for (let i = 1; i < 10000000000; i++) {}
+		return value * 2
+	}
+
+	const getFilteredData = useCallback(step => {
+		console.log('FIlter data')
+		return data.filter(item => item > 4)
 	}, [])
-	useEffect(() => {
-		if (counter > 3) {
-			console.log('STOP CLICKING ON MY BUTTON')
-		}
-	}, [counter])
-	useEffect(() => {
-		console.log('counter or step was changed')
-	}, [counter, step])
 
+	//1x01dr
+	//0dd101
+	//1xd01r
+
+	const resData = useMemo(() => {
+		return getFilteredData(step)
+	}, [step, getFilteredData])
+
+	const result = useMemo(() => {
+		return sum(step)
+	}, [step])
+	// const res = sum(step)
 	const increment = () => {
-		setName('Stepan')
-
-		setUser({
-			...user,
-			name: 'Andre',
-		})
 		setCounter(prev => prev + step)
 	}
 
@@ -47,19 +43,22 @@ export const Counter = () => {
 	}
 
 	const handleChangeStep = e => {
-		setStep(e.target.value)
+		setStep(+e.target.value)
 	}
 
 	return (
 		<FlexContainer>
 			<StyledCounter>
 				<h1>{counter}</h1>
+
 				<input type='text' value={step} onChange={handleChangeStep} />
+				<input type='text' value={name} onChange={e => setName(e.target.value)} />
 				<Flex>
 					<StyledButton onClick={decrement}>minus</StyledButton>
 					<StyledButton onClick={reset}>reset</StyledButton>
 					<StyledButton onClick={increment}>plus</StyledButton>
 				</Flex>
+				<h1>{result}</h1>
 			</StyledCounter>
 		</FlexContainer>
 	)
