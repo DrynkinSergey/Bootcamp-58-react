60d028030c63e94dca81105158646a8fc693b36d
Sergey <sergsmile29@gmail.com>
2023-09-20 11:03:55 +0300

    DOCS

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -7,15 +7,23 @@ import { Link, useLocation, useSearchParams } from 'react-router-dom'
 const Posts = () => {
 	const { register, handleSubmit } = useForm()
 
+	// 1. Використовуємо хук для зміни URL, отримуємо параметри, та функцію, котра ставить ці параметри
+	const [searchParams, setSearchParams] = useSearchParams()
+
+	// 2. При використанні сабміта, встановлюємо параметр query в наш url
 	const submit = data => {
 		setSearchParams(data.queryStr && { query: data.queryStr })
 	}
-	const [searchParams, setSearchParams] = useSearchParams()
+	// 3. Витягуємо query з нашого url через першу сущність хука useSearchParams
 	const query = searchParams.get('query') || ''
-	const location = useLocation()
-	console.log(location)
 
+	// 4. Робимо запит через власний хук useHttp, передаючи туди query в якості параметра пошуку
 	const { data } = useHttp(getPostsByQuery, query)
+
+	// 5. Створюємо об'єкт location, для того, щоб отримати поточний стан нашого url
+	// Це необхідно для передачі локейшен в компонент 'SinglePost'
+	const location = useLocation()
+
 	return (
 		<div>
 			<form onSubmit={handleSubmit(submit)}>
@@ -26,6 +34,12 @@ const Posts = () => {
 			<ol>
 				{data?.map(post => (
 					<li key={post.id}>
+						{/* // 6. Викоритовуємо новий пропс state в LINK для того щоб передати об'єкт локації
+            для компонента SinglePost.
+            І ми будемо мати змогу повернутись на останнє положення нашого location, де буде знаходитись
+             поле query.
+             =>>>>>> Йдемо до компонента singlePost і продовжуємо
+            */}
 						<Link state={{ from: location }} to={post.id.toString()}>
 							<h2>{post.title}</h2>
 						</Link>
--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -4,12 +4,22 @@ import { getPostById } from '../service/api'
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom'
 
 const SinglePost = () => {
+	// 1. Отримаємо постАйді через хук useParams
 	const { postId } = useParams()
+	// 2. Отримаємо один пост через власний хук, передаємо айді для пошуку
 	const { data } = useHttp(getPostById, postId)
+	// 3. Використовуємо useLocation, щоб отримати в стейт самої локації те, звідки ми прийшли!
+	// Якщо ми потрапимо на цю сторінку з нової - там буде null
+	// Тому треба заглушка '/'
 	const location = useLocation()
+	// 4. Створюємо реф, Для того, щоб записати отриманий об'єкт локації
+	// Реф не дасть перезатертись посиланню
+	// Також ставимо заглушку '/' і обов'язково оператор умовної перевірки (елвіс) ? перед from
 	const goBackRef = useRef(location.state?.from || '/')
+	console.log(location)
 	return (
 		<div>
+			{/* Використовуємо наш реф, щоб повернутись назад зі всіма параметрами */}
 			<Link to={goBackRef.current}>Go back</Link>
 			<h1>Post #{postId}</h1>
 			<hr />
