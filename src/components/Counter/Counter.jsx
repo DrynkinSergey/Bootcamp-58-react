a9bbc86af44a5b73222d6853e17fa949809d9863
Sergey <sergsmile29@gmail.com>
2023-09-22 01:32:27 +0300

    clear

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,96 +1,15 @@
-import React, { useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react'
 import { Flex, FlexContainer, StyledButton, StyledCounter } from './Counter.styled'
-const data = ['1', 2, 3, 4, 5, 6, 11]
 export const Counter = () => {
-	const initialState = {
-		counter: 0,
-		step: 1,
-	}
-
-	const counterReducer = (state, action) => {
-		console.log(action)
-		switch (action.type) {
-			case 'INCREMENT':
-				return {
-					...state,
-					counter: state.counter + state.step,
-				}
-			case 'decrement':
-				return {
-					...state,
-					counter: state.counter - state.step,
-				}
-			case 'RESET':
-				return {
-					...state,
-					counter: 0,
-					step: 1,
-				}
-			case 'SET_STEP':
-				return {
-					...state,
-					step: action.payload,
-				}
-			default:
-				return state
-		}
-	}
-
-	const [state, dispatch] = useReducer(counterReducer, initialState)
-
-	// const sum = value => {
-	// 	console.log('CALC SOME DATA')
-	// 	for (let i = 1; i < 10000000000; i++) {}
-	// 	return value * 2
-	// }
-
-	// const getFilteredData = useCallback(step => {
-	// 	console.log('FIlter data')
-	// 	return data.filter(item => item > 4)
-	// }, [])
-
-	//1x01dr
-	//0dd101
-	//1xd01r
-
-	// const resData = useMemo(() => {
-	// 	return getFilteredData(step)
-	// }, [step, getFilteredData])
-
-	// const result = useMemo(() => {
-	// 	return sum(step)
-	// }, [step])
-	// const res = sum(step)
-	const increment = () => {
-		dispatch({ type: 'INCREMENT' })
-		// setCounter(prev => prev + step)
-	}
-
-	const decrement = () => {
-		// setCounter(prev => prev - step)
-		dispatch({ type: 'decrement' })
-	}
-
-	const reset = () => {
-		// setCounter(0)
-		dispatch({ type: 'RESET' })
-	}
-
-	const handleChangeStep = e => {
-		// setStep(+e.target.value)
-		dispatch({ type: 'SET_STEP', payload: +e.target.value })
-	}
-
 	return (
 		<FlexContainer>
 			<StyledCounter>
-				<h1>{state.counter}</h1>
+				<h1>{0}</h1>
 
-				<input type='text' value={state.step} onChange={handleChangeStep} />
+				<input type='text' />
 				<Flex>
-					<StyledButton onClick={decrement}>minus</StyledButton>
-					<StyledButton onClick={reset}>reset</StyledButton>
-					<StyledButton onClick={increment}>plus</StyledButton>
+					<StyledButton>minus</StyledButton>
+					<StyledButton>reset</StyledButton>
+					<StyledButton>plus</StyledButton>
 				</Flex>
 			</StyledCounter>
 		</FlexContainer>
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -6,51 +6,34 @@ import { WrapperPosts } from './Posts.styled'
 import { Button } from './Button'
 import { Spinner } from './Spinner'
 import { toast } from 'react-toastify'
-import { initialState, postReducer } from '../../reducer/postReducer'
-
-import { changeQuery, nextPage, setLimit, setLoading, setPosts } from '../../reducer/actions'
 
 const Posts = () => {
-	const [state, dispatch] = useReducer(postReducer, initialState)
+	// useEffect(() => {
+	// 	const getPostsFn = async fnType => {
 
-	const { posts, skip, limitValue, loading, error, query } = state
+	// 		try {
+	// 			const { posts, limit } = await fnType({ limit: limitValue, skip, q: query })
+	// 		} catch (error) {
+	// 			alert(error.message)
+	// 		} finally {
+	// 			toast.success('You data is ready!')
+	// 		}
+	// 	}
+	// 	if (query) {
+	// 		getPostsFn(fetchPostsByQuery)
+	// 	} else {
+	// 		getPostsFn(fetchPosts)
+	// 	}
+	// }, [])
 
-	useEffect(() => {
-		const getPostsFn = async fnType => {
-			dispatch(setLoading(true))
-
-			try {
-				const { posts, limit } = await fnType({ limit: limitValue, skip, q: query })
-				dispatch(setPosts(posts))
-				dispatch(setLimit(limit))
-			} catch (error) {
-				alert(error.message)
-			} finally {
-				toast.success('You data is ready!')
-				dispatch(setLoading(false))
-			}
-		}
-		if (query) {
-			getPostsFn(fetchPostsByQuery)
-		} else {
-			getPostsFn(fetchPosts)
-		}
-	}, [limitValue, skip, query])
-
-	const handleLoadMore = () => {
-		dispatch(nextPage())
-	}
-	const handleChangeQuery = queryStr => {
-		dispatch(changeQuery(queryStr))
-	}
 	return (
 		<>
-			<Header onChangeQuery={handleChangeQuery} />
+			<Header />
 			<WrapperPosts>
-				<h1>Your query is:{query} </h1>
-				{loading && !posts.length ? <Spinner /> : <PostList posts={posts} />}
-				<Button disabled={loading} onClick={handleLoadMore} $bg='teal'>
-					{loading ? 'Loading...' : 'Load more'}
+				<h1>Your query is: </h1>
+				<PostList posts={[]} />
+				<Button disabled={'loading'} $bg='teal'>
+					Load more
 				</Button>
 			</WrapperPosts>
 		</>
