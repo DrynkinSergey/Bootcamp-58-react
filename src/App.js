6434c445d930a2a2345ec3558b4c5bc5b906a2b9
Sergey <sergsmile29@gmail.com>
2023-09-29 00:06:14 +0300

    clear

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Posts from './components/Posts/Posts'
 import Todolist from './components/Todolist/Todolist'
 
 const App = () => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { todoReducer } from './todoList/slice'
-import { postsReducer } from './posts/slice'
 
 export const store = configureStore({
 	reducer: {
 		todos: todoReducer,
-		posts: postsReducer,
 	},
 	middleware: getDefaultMiddleware => getDefaultMiddleware(),
 	devTools: process.env.NODE_ENV !== 'production',
